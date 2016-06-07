bda9f9d30101a1619c0c5a04ee69b25d7abfbc28
Matthew Parvinsmith <mrparvinsmith@gmail.com>
2016-06-07 03:17:58 -0700

    changed mongoUrl and package.json to match app

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
-// change the link below!
-var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/my-boilerplate';
+var mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/simple-business-card';
 
 mongoose.connect(mongoUrl, function(err){
   if(err) throw err;
